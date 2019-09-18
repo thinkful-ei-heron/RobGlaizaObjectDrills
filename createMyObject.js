3b7b4b67cf8d753c956158d3431bea54219e9c92
robarcand <robbyarcand@aol.com>
2019-09-18 12:15:39 -0400

    added createMyObject.js

---
+++ createMyObject.js
@@ -0,0 +1,11 @@
+'use strict';
+const obj = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+        return 'hello';
+    }
+};
+
+console.log(obj);
