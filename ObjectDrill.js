ec79d0c79572884d8be45a3c5b7eff58961cd4ee
Glaiza Wagner <glaizawagner@gmail.com>
2019-09-18 12:45:28 -0700

    #3 Array in objects

--- ObjectDrill.js
+++ ObjectDrill.js
@@ -1,27 +1,34 @@
-//#1
-const loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function() {
-        return (this.water / this.flour) * 100;
-    }
+// //#1
+// const loaf = {
+//     flour: 300,
+//     water: 210,
+//     hydration: function() {
+//         return (this.water / this.flour) * 100;
+//     }
+// };
+// const result = loaf.hydration()
+// console.log(result);
+
+// //#2
+
+// let result2 = "";
+// const myObject = {
+//     foo: 2,
+//     bar: 4,
+//     fum: 6,
+//     quux: 8,
+//     spam: 10
+// }
+
+// for (let val in myObject) {
+//     result2 += myObject[val];
+// }
+
+// console.log(result2);
+
+//3 Arrays in Objects
+const meal = {
+    meals:['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 };
-const result = loaf.hydration()
-console.log(result);
 
-//#2
-
-let result2 = "";
-const myObject = {
-    foo: 2,
-    bar: 4,
-    fum: 6,
-    quux: 8,
-    spam: 10
-}
-
-for (let val in myObject) {
-    result2 += myObject[val];
-}
-
-console.log(result2);
+console.log(meal.meals[3]);
