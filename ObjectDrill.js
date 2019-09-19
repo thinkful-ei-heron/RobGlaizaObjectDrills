25b95283fb2cdf2f8c09ad07123b1da2c3475d10
Glaiza Wagner <glaizawagner@gmail.com>
2019-09-18 20:20:49 -0700

    bonus a

--- ObjectDrill.js
+++ ObjectDrill.js
@@ -191,3 +191,38 @@ function createCharacter(name, nickName, race, origin, attack, defense, weapon) 
   });
   
 console.log(characters[0].describe());
+
+//Bonus A
+
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query) {
+let result = '';
+let obj;
+
+  for (let i = 0; i < arr.length; i++) {
+    obj = arr[i];
+    for (let item in query) {
+        if (obj[item] === query[item]) {
+          result = obj;
+        } else {
+          result = null;
+          break;
+        }
+    }
+    if (result !== null) {
+      return obj;
+    } 
+  }
+  return null;
+}
+
+findOne(HEROES, {id: 2, name:'Aquaman'});
